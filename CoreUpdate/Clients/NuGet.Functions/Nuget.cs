a793ddad7c8979176a86312bf4c7c190de6c03dc
Henrik Hallenberg <HNB@energinet.dk>
2024-08-15 00:23:36 +0200

    test

--- CoreUpdate/Clients/NuGet.Functions/Nuget.cs
+++ CoreUpdate/Clients/NuGet.Functions/Nuget.cs
@@ -1,6 +1,3 @@
-using System.IO;
-using System.Net;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -10,31 +7,37 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 using NuGet.Service.Core;
+using NuGet.Versioning;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace NuGet.Functions
 {
     public class Nuget
     {
-        private readonly ILogger<Nuget> _logger;
-
+        private readonly ILogger<Nuget> Logger;
+        public required IServerPackageRepository ServerRepository;
+        
         public Nuget(ILogger<Nuget> log, IServerPackageRepository serverRepository)
         {
-            _logger = log;
+            Logger = log;
             ServerRepository = serverRepository;
         }
 
-        private required IServerPackageRepository ServerRepository { get; }
 
         [FunctionName("Packages")]
-        [OpenApiOperation(operationId: "Packages")]
+        [OpenApiOperation(operationId: "Packages", tags: new[] { "semVerLevel" })]
+        [OpenApiParameter(name: "semVerLevel", In = ParameterLocation.Query, Required = false, Type = typeof(string), Description = "The **semVerLevel** parameter")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
         public async Task<IActionResult> Packages(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, CancellationToken token = default(CancellationToken))
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            Logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            //string name = req.Query["name"];
-            ServerRepository.GetPackagesAsync();
+            var clientCompatibility = SemanticVersion.TryParse(req.Query["semVerLevel"], out var parseOutput) ? new ClientCompatibility(parseOutput) : ClientCompatibility.Default;
+            var resoult = await ServerRepository.GetPackagesAsync(clientCompatibility, token);
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
 
