28173d2c18a81dbc9a169554f08c783eecdc5f57
Henrik Hallenberg <HNB@energinet.dk>
2024-08-26 14:42:14 +0200

    furt step

--- CoreUpdate/Clients/NuGet.Functions/NugetPackage.cs
+++ CoreUpdate/Clients/NuGet.Functions/NugetPackage.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System.Net;
@@ -22,6 +23,7 @@ public class NugetPackage
 
     [FunctionName(nameof(GetPackagesAsync))]
     [OpenApiOperation(operationId: "Packages")]
+    [OpenApiParameter("id", Description = "The package name")]
     [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
     public async Task<IActionResult> GetPackagesAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "v3-Packages/{id}/index.josn")]
         HttpRequest req,
@@ -35,6 +37,9 @@ public class NugetPackage
 
     [FunctionName(nameof(GetPackageAsync))]
     [OpenApiOperation(operationId: "Packages")]
+    [OpenApiParameter("id", Description = "The package name")]
+    [OpenApiParameter("version", Description = "The package version")]
+    [OpenApiParameter("combined", Description = "Combined id.version")]
     [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
     public async Task<IActionResult> GetPackageAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "v3-Packages/{id}/{version}/{combined}.nupkg")]
         HttpRequest req,
@@ -43,6 +48,10 @@ public class NugetPackage
         string combined,
         CancellationToken token = default)
     {
+        if (!combined.Equals($"{id}.{version}"))
+        {
+            return new NotFoundObjectResult("id and version don't match requested nupkg");
+        }
         await Task.CompletedTask;
 
         return new OkResult();
@@ -50,14 +59,21 @@ public class NugetPackage
 
     [FunctionName(nameof(GetPackageManifestAsync))]
     [OpenApiOperation(operationId: "Packages")]
+    [OpenApiParameter("id", Description = "The package name")]
+    [OpenApiParameter("version", Description = "The package version")]
+    [OpenApiParameter("name", Description = "The package name same as the Id")]
     [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
-    public async Task<IActionResult> GetPackageManifestAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "v3-Packages/{id}/{version}/{combined}.nuspec")]
+    public async Task<IActionResult> GetPackageManifestAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "v3-Packages/{id}/{version}/{name}.nuspec")]
         HttpRequest req,
         string id,
         string version,
-        string combined,
+        string name,
         CancellationToken token = default)
     {
+        if (!name.Equals($"{id}"))
+        {
+            return new NotFoundObjectResult("id don't match");
+        }
         await Task.CompletedTask;
 
         return new OkResult();
