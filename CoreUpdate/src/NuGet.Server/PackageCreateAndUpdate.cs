cc3e86c86233e3d3aa6dbcd8ed41442f82d19f95
Henrik Hallenberg <HNB@energinet.dk>
2024-08-27 01:15:55 +0200

    read ZipFile

---
+++ CoreUpdate/src/NuGet.Server/PackageCreateAndUpdate.cs
@@ -0,0 +1,38 @@
+﻿using NuGet.Service.Core;
+using System.IO.Compression;
+
+namespace NuGet.Service;
+public class PackageCreateAndUpdate : IPackageCreateAndUpdate
+{
+    PackageCreateAndUpdate (INugetPackageCRUD storeage)
+    {
+        Storeage = storeage;
+    }
+
+    private const string nusoec = ".nuspec";
+
+    INugetPackageCRUD Storeage { get; set; }
+
+    public Task DeletePackageAsync(string packageName, string packageVersion)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RelistPackageAsync(string packageName, string packageVersion)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UploadPackageAsync(Stream stream)
+    {
+        using var arciv = new ZipArchive(stream);
+        foreach (ZipArchiveEntry entry in arciv.Entries)
+        {
+            if(entry.FullName.EndsWith(nusoec, StringComparison.OrdinalIgnoreCase))
+            {
+                using StreamReader reader = new StreamReader(entry.Open());
+                string text = await reader.ReadToEndAsync();
+            }
+        }
+    }
+}
